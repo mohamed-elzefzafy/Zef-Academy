9eb4dc463758818249de1c6353021ec6c56df4ea
unknown <mohamedelzefzafy@gmail.com>
2025-08-17 02:46:29 +0300

    prepare to deploy-5

--- zef-academy-frontend/src/app/(global-routes)/(course-routes)/course/[courseId]/page.tsx
+++ zef-academy-frontend/src/app/(global-routes)/(course-routes)/course/[courseId]/page.tsx
@@ -23,18 +23,56 @@
 
 
 
-import axiosRequest from "@/utils/request";
+// import axiosRequest from "@/utils/request";
+// import { ICourse } from "@/types/course";
+// import CourseContent from "./_components/CourseContent";
+
+// const CoursePage = async ({ params }: { params: { courseId: string } }) => {
+//   if (!params.courseId) return null;
+
+//   const { data: course } = await axiosRequest.get<ICourse>(
+//     `/api/v1/course/${params.courseId}`
+//   );
+
+//   console.log(course);
+
+//   return <CourseContent course={course} />;
+// };
+
+// export default CoursePage;
+
+
+// import axiosRequest from "@/utils/request";
+// import { ICourse } from "@/types/course";
+// import CourseContent from "./_components/CourseContent";
+// import { PageProps } from "next"; // ✨ خد التايب الجاهز من Next.js
+
+// const CoursePage = async ({ params }: PageProps<{ courseId: string }>) => {
+//   if (!params?.courseId) return null;
+
+//   const { data: course } = await axiosRequest.get<ICourse>(
+//     `/api/v1/course/${params.courseId}`
+//   );
+
+//   return <CourseContent course={course} />;
+// };
+
+// export default CoursePage;
+
+
+
 import { ICourse } from "@/types/course";
+import axiosRequest from "@/utils/request";
 import CourseContent from "./_components/CourseContent";
 
-const CoursePage = async ({ params }: { params: { courseId: string } }) => {
-  if (!params.courseId) return null;
+interface PageProps {
+  params: { courseId: string };
+}
 
+const CoursePage = async ({ params }: PageProps) => {
   const { data: course } = await axiosRequest.get<ICourse>(
     `/api/v1/course/${params.courseId}`
   );
-
-  console.log(course);
 
   return <CourseContent course={course} />;
 };
