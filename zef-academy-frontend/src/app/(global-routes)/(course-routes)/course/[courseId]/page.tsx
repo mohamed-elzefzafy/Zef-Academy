6023811b0710706b2da9bbc1fa82ef4f21c206b3
unknown <mohamedelzefzafy@gmail.com>
2025-08-17 03:54:35 +0300

    prepare to deploy-9

--- zef-academy-frontend/src/app/(global-routes)/(course-routes)/course/[courseId]/page.tsx
+++ zef-academy-frontend/src/app/(global-routes)/(course-routes)/course/[courseId]/page.tsx
@@ -1,32 +1,32 @@
-"use client";
-import CourseContent from "./_components/CourseContent";
-import { use } from "react";
-import { useGetOneCourseQuery } from "@/redux/slices/api/courseApiSlice";
-import Loading from "@/app/loading";
+// "use client";
+// import CourseContent from "./_components/CourseContent";
+// import { use } from "react";
+// import { useGetOneCourseQuery } from "@/redux/slices/api/courseApiSlice";
+// import Loading from "@/app/loading";
 
 
-const CoursePage =  ({
-  params,
-}: {
-  params: Promise<{ courseId: string }>;
-}) => {
-  const resolvedParams = use(params);
+// const CoursePage =  ({
+//   params,
+// }: {
+//   params: Promise<{ courseId: string }>;
+// }) => {
+//   const resolvedParams = use(params);
 
 
-  const {data : course , isLoading} = useGetOneCourseQuery(resolvedParams.courseId);
+//   const {data : course , isLoading} = useGetOneCourseQuery(resolvedParams.courseId);
 
- if (isLoading) {
-  return <Loading/>
- }
+//  if (isLoading) {
+//   return <Loading/>
+//  }
 
-  if (!course) {
-  return 
- }
+//   if (!course) {
+//   return 
+//  }
 
-  return <CourseContent course={course} />;
-};
+//   return <CourseContent course={course} />;
+// };
 
-export default CoursePage;
+// export default CoursePage;
 
 
 
@@ -55,3 +55,65 @@ export default CoursePage;
 // };
 
 // export default CoursePage;
+
+
+
+
+
+
+
+// "use client";
+// import CourseContent from "./_components/CourseContent";
+// import { useGetOneCourseQuery } from "@/redux/slices/api/courseApiSlice";
+// import Loading from "@/app/loading";
+
+// const CoursePage = ({ params }: { params: { courseId: string } }) => {
+//   const { data: course, isLoading } = useGetOneCourseQuery(params.courseId);
+
+//   if (isLoading) {
+//     return <Loading />;
+//   }
+
+//   if (!course) {
+//     return <div>Course not found</div>;
+//   }
+
+//   return <CourseContent course={course} />;
+// };
+
+// export default CoursePage;
+
+
+
+
+"use client";
+
+import CourseContent from "./_components/CourseContent";
+import { use } from "react";
+import { useGetOneCourseQuery } from "@/redux/slices/api/courseApiSlice";
+import Loading from "@/app/loading";
+
+const CoursePage = ({
+  params,
+}: {
+  params: Promise<{ courseId: string }>;
+}) => {
+  // نفك الـ Promise بتاع params
+  const resolvedParams = use(params);
+
+  const { data: course, isLoading } = useGetOneCourseQuery(
+    resolvedParams.courseId
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!course) {
+    return <div>Course not found</div>;
+  }
+
+  return <CourseContent course={course} />;
+};
+
+export default CoursePage;
