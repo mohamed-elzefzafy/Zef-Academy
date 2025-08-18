4f08cae4e61166e619f88af70e535b66d0bbaa27
unknown <mohamedelzefzafy@gmail.com>
2025-08-19 01:56:00 +0300

    deploy-16

--- zef-academy-frontend/src/app/checkout/Checkout.tsx
+++ zef-academy-frontend/src/app/checkout/Checkout.tsx
@@ -1,15 +1,17 @@
 "use client";
 import { Button } from "@mui/material";
 import React from "react";
-import checkout from "./actions/checkout";
+// import checkout from "./actions/checkout";
 import getStripe from "./stripe";
+import { useCheckoutStripeMutation } from "@/redux/slices/api/checkoutiSlice";
 
 interface CheckoutProps {
   courseId: string;
 }
 const Checkout = ({ courseId }: CheckoutProps) => {
+  const [checkoutStripe] = useCheckoutStripeMutation();
   const handleCheckout = async () => {
-    const session = await checkout(courseId);
+    const session = await checkoutStripe({courseId}).unwrap();
     const stripe = await getStripe();
     await stripe?.redirectToCheckout({ sessionId: session.id });
   };
